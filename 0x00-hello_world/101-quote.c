3ac298a1d23aba4f0d47732c80ab753af115bebf
dedo <didos312@gmail.com>
2023-02-20 20:59:50 +0200

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints out the last part of a quote in the standard error
- *return: 1 if success
+ * return: 1 if success
  */
 int main(void)
 {
-char quote[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 fprintf(stderr, "%s\n", quote);
 return (1);
