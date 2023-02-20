693fb67e8b57f3baec484ab239f2e419be38fc80
dedo <didos312@gmail.com>
2023-02-20 20:38:01 +0200

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- *main -entry point
+ *main - entry point
  *
- *prints the last part of a quote in the stderr using function fprintf() which print in the stderr
+ *main - prints the last part of a quote in the stderr using functio
  *return = 1 if true
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
