e0b1d18e4a4d783b9b984dcb0326b69209d73ad9
dedo <didos312@gmail.com>
2023-02-22 01:14:01 +0200

    f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
  * main - a for loop to print the word putchar
- * Return value  always 0
+ * Return value  always = 0
  */
 
 int main(void)
 {
 
 	char word[] = "_putchar";
+
 	int i;
+
 	for (i = 0; i < 8; i++)
 
 		_putchar(word[i])
