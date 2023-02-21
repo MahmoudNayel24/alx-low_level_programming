0a66a0a07667fbd094d8f4a324dfb526c499a272
dedo <didos312@gmail.com>
2023-02-22 01:26:17 +0200

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints alphabet
+ *
+ * Return : value always = 0
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 97;i <= 122;i++)
+		_putchar(i);
+	_putchar('\n');
+
+
+	return (0);
+}
