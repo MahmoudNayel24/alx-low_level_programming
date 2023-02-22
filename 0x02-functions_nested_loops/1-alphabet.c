4c2a6f97b2349822844ed5b1de281d9adc37dbfa
dedo <didos312@gmail.com>
2023-02-22 02:00:28 +0200

    f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * this is a funcion with main function i don't know why
  * print_alphabet : as said it prints alphabetfasfs fasf
  * Return : value always = 0
+ * initial short description : kos om el betty documentation style
  */
+
 
 void print_alphabet(void)
 {
