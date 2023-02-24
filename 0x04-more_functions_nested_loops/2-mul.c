9011344dfd6bf624c8ab14ddcbf28d4d2c979596
dedo <didos312@gmail.com>
2023-02-24 22:42:48 +0200

    jj

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiply 2 parameters together
+ *
+ * @a: argument 1
+ * @b: argument 2
+ * Return: equal a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
