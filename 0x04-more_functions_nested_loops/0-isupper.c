158f69d0a51e9cfe7c05bc5283954f0b9722ad4f
dedo <didos312@gmail.com>
2023-02-24 21:49:29 +0200

    f

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper checks wether the letter is uppercase or not
+ *
+ * Return : equal 1 if ture and 0 if not
+ */
+
+int _isupper(int c)
+{
+
+	if (isupper(c))
+		return 1;
+	else
+		return 0;
+}
