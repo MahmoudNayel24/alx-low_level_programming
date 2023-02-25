02ad7dfe0de8805d092524867395fee144bbf6bf
dedo <didos312@gmail.com>
2023-02-25 07:00:18 +0200

    f

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,18 @@ int main(void)
 	for (x = 1; x <= 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
-			printf("FizzBuzz%c", ' ');
+			printf("FizzBuzz ");
 		else if (x % 3 == 0)
-			printf("Fizz%c", ' ');
+			printf("Fizz ");
 		else if (x % 5 == 0)
-			printf("Buzz%c", ' ');
+		{
+			if (x == 100)
+				printf("Buzz \n");
+			else
+				printf("Buzz ");
+		}
 		else
-			printf("%d%c", x, ' ');
+			printf("%d ", x);
 	}
 
 	return (0);
