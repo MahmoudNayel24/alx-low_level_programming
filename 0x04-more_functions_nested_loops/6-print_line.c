3b2f4b557099264b88015e1e8c6f36848a654916
dedo <didos312@gmail.com>
2023-02-25 07:11:23 +0200

    f

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,8 +12,11 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i = 0; i < n; i++)
+	else
+	{
+		for (i = 0; i < n; i++)
 
-		_putchar('_');
-	_putchar('\n');
+			_putchar('_');
+	}
+		_putchar('\n');
 }
