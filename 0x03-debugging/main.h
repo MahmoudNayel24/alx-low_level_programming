056bb8c589299f7591473efe865df3312fec9798
dedo <didos312@gmail.com>
2023-02-26 22:13:27 +0200

    f

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-
+void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
 int _putchar(char c);
 int _isdigit(int c);
